9902bfb00e186764626c74395beffc2522327638
DanielWent <109043916+DanielWent@users.noreply.github.com>
2026-08-30 20:11:12 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -109,7 +109,7 @@ async def sync(email: str, password: str, start_date: date, end_date: date, outp
         file_prefix = "aflw_"
 
     fields_to_validate = [
-        'average_stress', 'steps', 'floors_climbed', 'total_calories', 'intensity_minutes',
+        'average_stress', 'average_waking_stress', 'steps', 'floors_climbed', 'total_calories', 'intensity_minutes',
         'body_battery_min', 'body_battery_max', 'body_battery_charged', 'body_battery_drained',
         'moderate_intensity_minutes', 'vigorous_intensity_minutes', 'active_calories',
         'rest_stress_duration', 'low_stress_duration', 'medium_stress_duration', 'high_stress_duration'
